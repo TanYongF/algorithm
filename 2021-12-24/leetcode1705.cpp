23283680e8a1d9cd3a2374d13db44e31cc38c471
B19040229谭永锋 <tan13621251388@gmail.com>
2021-12-24 23:09:15 +0800

    日常更新

---
+++ 2021-12-24/leetcode1705.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define ms(s,val) memset(s, val, sizeof(s))
+const int inf = INT_MAX;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int eatenApples(vector<int>& apples, vector<int>& days) {
+        int len = apples.size();
+        map<int,int> mp;
+    	int curday = 1, ans;
+        map<int,int>::iterator eat;
+        for(int i = 0; i < len; i++){
+        	mp[days[i] + i] += apples[i];
+ 			if((eat = mp.upper_bound(curday)) != mp.end()){
+ 				ans++;
+ 				eat->second--;
+ 				if(eat->second == 0) mp.erase(eat);
+ 			} 	
+        	curday++;
+        }
+
+        //处理后面天数情况
+        while((eat = mp.upper_bound(curday)) != mp.end()){
+        	ans++;
+        	eat->second--;
+        	if(eat->second == 0) mp.erase(eat);
+        	curday++;
+        }   
+        return ans;
+       
+
+    }
+
+};
+int main(int argc, char * argv[]){
+
+	Solution s = Solution();
+	/* code */
+
+
+    return 0;
+}
