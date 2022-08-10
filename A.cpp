d255a5042a1fd958edca12332df764c318f873c1
B19040229谭永锋 <tan13621251388@gmail.com>
2022-08-10 23:36:28 +0800

    upate

--- A.cpp
+++ A.cpp
@@ -15,60 +15,35 @@ struct TreeNode {
 
 class Solution {
 public:
-    int xx[4] = {1, 0, -1, 0}, yy[4] = {0, 1, 0, -1};
-    unordered_map<int, int> mp;
-    int idx = -1, m, n, area = 0;
-    void dfs(vector<vector<int>> &grid, int cx, int cy){
-        area++;
-        grid[cx][cy] == idx;
-        for(int i = 0; i < 4; i++){
-            int nx = cx + xx[i], ny = cy + yy[i];
-            if(nx < m and nx >= 0 and ny < n and ny >= 0 and grid[nx][ny] == 1) dfs(grid, nx, ny);
-        }
-    }
-    int largestIsland(vector<vector<int>>& grid) {
-        mp[0] = 0;
-        m = grid.size(), n = grid[0].size();
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    area = 0;
-                    dfs(grid, i, j);
-                    mp[idx--] = area;
-                } 
+    string removeKdigits(string num, int k) {
+        int len = num.size();
+        if(k == len) return 0;
+        priority_queue<pair<int,int>> pq;
+        int del = 0, end = 0;
+        while(del < k){
+            while(end < len and pq.size() < k){
+                pq.push({num[end] - '0', end});
+                end++;
             }
+            auto [val, idx] = pq.top();
+            pq.pop();
+            num[idx] = ' ';
+            del++;
         }
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                cout << grid[i][j] <<  " ";
-            }
-            cout << endl;
+        string ans = 0;
+        for(auto &ch : num){
+            if(ch != ' ') ans.push_back(ch);
         }
-        
-        int ans = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] != 0) continue;
-                unordered_map<int,int> can;
-                if(i-1 >= 0) can[grid[i-1][j]] += mp[grid[i-1][j]];
-                if(i+1 <  m) can[grid[i+1][j]] += mp[grid[i+1][j]];
-                if(j-1 >= 0) can[grid[i][j-1]] += mp[grid[i][j-1]];
-                if(j+1 <  n) can[grid[i][j+1]] == mp[grid[i][j+1]];
-                int bindingArea = 0;
-                for(auto it : can) bindingArea += it.second;
-                ans = max(ans, bindingArea);
-            }
-        }
+        cout << ans;
         return ans;
     }
 };
 int main(int argc, char * argv[]){
 
 	Solution s = Solution();
-	vector<vector<int>> test = {{1,0}, {0, 1}};
-	return s.largestIsland(test);
-	/* code */
-
-
+	string str;
+	int k;
+	cin >> str >> k;
+	cout << s.removeKdigits(str, k);
     return 0;
 }
